e45b438c206191b114e64778577d8b6761e7dee6
dp7sv <dp7sv@virginia.edu>
2019-10-27 15:00:58 -0400

    fixed a typo in products.php

--- product.php
+++ product.php
@@ -4,8 +4,8 @@ session_start();
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
-$_SESSION['email'] = $_POST['username'];
-$_SESSION['product'] = $_POST['password'];
+$_SESSION['email'] = $_POST['email'];
+$_SESSION['product'] = $_POST['product'];
 
 $email = $_SESSION['email'];
 $product = $_SESSION['product'];
