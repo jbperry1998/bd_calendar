4ac99d5df5c142fbcf935968a51a086064c15be8
dp7sv <dp7sv@virginia.edu>
2019-11-04 21:35:19 -0500

    properly capitalized SESSION in product.php

--- log_in.php
+++ log_in.php
@@ -11,8 +11,8 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 //password probably is incorrect column name
 $query = "SELECT username FROM site_users WHERE password='$hashed_password' AND email='$email'"; 
 $result = pg_query($db_connection,$query);
-$usernames = pg_fetch_row($result);
 $entries = pg_fetch_assoc($result);
+$usernames = pg_fetch_row($result);
 
 
 if($entries) {
--- product.php
+++ product.php
@@ -4,18 +4,8 @@ session_start();
 $db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 ");
-//$_SESSION['email'] = $_POST['email'];
-//$_SESSION['product'] = $_POST['product'];
 
-//$email = $_SESSION['email'];
-//$product = $_SESSION['product'];
-
-$login_status = $_Session['logged_in'];
-
-//$query = "SELECT* FROM site_users WHERE email='$email'";
-//$result = pg_query($db_connection,$query);
-//$user = pg_fetch_assoc($result);
-
+$login_status = $_SESSION['logged_in'];
 
 if($login_status == 1) {
     
