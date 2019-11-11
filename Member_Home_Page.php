5e1c2274048c4aafeaa862bb0310308e7eb8d4e4
dp7sv <dp7sv@virginia.edu>
2019-11-11 11:14:18 -0500

    capitalized e

--- Member_Home_Page.php
+++ Member_Home_Page.php
@@ -73,7 +73,7 @@
 								$db_connection = pg_connect("host=ec2-174-129-227-80.compute-1.amazonaws.com
  								port=5432 dbname=dbvs140f5cqkp1 user=zdlwovjrekrdar password=ea1a662a2d7df06996a35f5aee8b2ac1d852cbe10af9af3c5cc60b41ee0d21f5
 								");
-								$query = "SELECT * FROM sales WHERE email='$email'";
+								$query = "SELECT * FROM sales WHERE Email='$email'";
 								$result = pg_query($db_connection, $query);
 								echo "<table border='1'>
 								<tr>
